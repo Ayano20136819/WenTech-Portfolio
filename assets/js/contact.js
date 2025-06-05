0e9c669c0da620055402351e3e12fdace117fec7
20136819 <20136819@tafe.wa.edu.au>
2025-06-05 11:51:39 +0800

    feat(projects_page): add project page

---
+++ assets/js/contact.js
@@ -0,0 +1,46 @@
+// Form Validation
+const nameRegex = /^[a-zA-Z]$/;
+const emailRegex = /^([a-zA-Z0-9._%+-]+)@([a-zA-Z0-9.-]+)\.([a-zA-Z]{2,})$/;
+
+const btn = document.getElementById("submitBtn");
+
+function checkForm() {
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  document.getElementById("name-error").textContent = "";
+  document.getElementById("email-error").textContent = "";
+  document.getElementById("message-error").textContent = "";
+
+  let isValid = true;
+
+  const result = document.createElement("p");
+
+  if (name === "" || !nameRegex.test(name)) {
+    document.getElementById("name-error").textContent = "Name is required.";
+    isValid = false;
+  }
+
+  if (email === "" || !emailRegex.test(email)) {
+    document.getElementById("email-error").textContent =
+      "Vaild email is required.";
+    isValid = false;
+  }
+
+  if (message === "") {
+    document.getElementById("message-error").textContent =
+      "Message cannot be empty.";
+    isValid = false;
+  }
+
+  return isValid;
+}
+
+btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const valid = checkForm();
+  if (valid) {
+    console.log("Form is valid. Submit logic goes here.");
+  }
+});
